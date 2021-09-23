95046d6927d0c60fd95346f4d75451c2cdde39f8
statizarm <statiz.arm@gmail.com>
2021-09-23 06:53:59 -0700

    hw1: sort formal verification - Failed to verify with permut

--- src/bubble.c
+++ src/bubble.c
@@ -14,18 +14,28 @@
   @			first <= i < last && first <= j < last && swap{L1, L2}(a, i, j) ==>
   @				permut{L1, L2}(a, first, last);
   @ }
-  @ predicate swap{L1, L2}(int *a, integer i, integer j) =
+  @*/
+
+/*@ predicate swap{L1, L2}(int *a, integer i, integer j) =
   @	\at(a[i], L1) == \at(a[j], L2) &&
   @	\at(a[j], L1) == \at(a[i], L2) &&
   @	\forall integer k; k != i && k != j ==> \at(a[k], L1) == \at(a[k], L2);
+  @
   @ predicate sorted(int *a, integer first, integer last) =
   @	\forall integer i;
   @		(first < i < last ==> a[i - 1] <= a[i]);
-  @
-  @ predicate greatest(int *a, integer j) =
-  @	 \forall integer i;
-  @ 		0 <= i < j ==> a[i] <= a[j];
   @*/
+
+/*@ requires \valid(a + i) && \valid(a + j);
+  @ assigns a[i], a[j];
+  @ ensures swap{Old, Here}(a, i, j);
+  @*/
+void swap(int *a, int i, int j) {
+	int tmp = a[i];
+	a[i] = a[j];
+	a[j] = tmp;
+}
+
 /*@ requires n > 0;
   @ requires \valid(a + (0 .. n-1));
   @ 
@@ -38,29 +48,29 @@ void bubble_sort(int *a, int n) {
 	int i, j, tmp;
 	//@ ghost int g_swap = 0;
 	/*@ loop invariant 0 <= i <= n - 1;
+	  @ loop invariant 0 <= g_swap <= 1;
 	  @ loop invariant sorted(a, n - i - 1, n);
-	  @ loop invariant permut{Pre, Here}(a, 0, n);
-	  @ loop assigns i, j, tmp, a[0..n-1];
+	  @ loop invariant 0 < i < n ==> \forall int k, l; 0 <= k <= n - i - 1 <= l < n ==> a[k] <= a[l];
+	  @ loop invariant 0 < i < n ==> permut{Pre, Here}(a, 0, n);
+	  @ loop assigns i, j, a[0..n-1];
 	  @ loop variant n - i;
 	  @*/
 	for (i = 0; i < n - 1; ++i) {
 		//@ ghost g_swap = 0;
 		/*@ loop invariant bound: 0 <= j <= n - i - 1;
-		  @ loop invariant 0 < j < n - i ==> greatest(a, j);
+		  @ loop invariant 0 <= g_swap <= 1;
+		  @ loop invariant 0 < j < n - i ==> \forall int k; 0 <= k < j ==> a[k] <= a[j];
 		  @ loop invariant 0 < j < n - i ==> (g_swap == 1) ==> permut{Pre, Here}(a, 0, n);
 		  @
-		  @ loop assigns j, tmp, a[0..n-i-1];
+		  @ loop assigns j, a[0..n-i-1];
 		  @ loop variant n - i - 1 - j;
 		  @*/
 		for (j = 0; j < n - i - 1; j++) {
 			if (a[j] > a[j + 1]) {
 				//@ ghost g_swap = 1;
-				tmp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = tmp;
+				swap(a, j, j + 1);
 			}
 		}
-		//@ assert greatest(a, n - i - 1);
 	}
 
 	//@ assert sorted(a, n - i - 1, n);
