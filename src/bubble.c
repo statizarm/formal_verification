e2d37ada6a43e4d6e2b26e2b527a0a0be44e4bcc
statizarm <statiz.arm@gmail.com>
2021-09-25 08:38:34 -0700

    hw1: sort formal verification - Trying to verify with multiset equal

--- src/bubble.c
+++ src/bubble.c
@@ -1,74 +1,71 @@
-/*@ inductive permut{L1, L2}(int *a, integer first, integer last) {
-  @ 	case permut_refl{L}:
-  @		\forall int *a, integer first, integer last;
-  @			permut{L, L}(a, first, last);
-  @	case permut_sym{L1, L2}:
-  @		\forall int *a, integer first, integer last;
-  @                     permut{L1, L2}(a, first, last) ==> permut{L2, L1}(a, first, last);
-  @	case permut_trans{L1, L2, L3}:
-  @		\forall int *a, integer first, integer last;
-  @			permut{L1, L2}(a, first, last) && permut{L2, L3}(a, first, last) ==>
-  @				permut{L1, L3}(a, first, last);
-  @	case permut_swap{L1, L2}:
-  @		\forall int *a, integer first, i, j, last;
-  @			first <= i < last && first <= j < last && swap{L1, L2}(a, i, j) ==>
-  @				permut{L1, L2}(a, first, last);
+/*@ axiomatic COUNT {
+  @ 	logic integer count{L}(int *a, int num, integer n);
+  @	axiom count_empty{L}:
+  @		\forall int *a, num, integer n;
+  @			n == 0 ==> count{L}(a, num, n) == 0;
+  @	axiom count_matched_last{L}:
+  @		\forall int *a, num, integer n;
+  @			\at(a[n - 1], L) == num ==> count{L}(a, num, n) == count{L}(a, num, n - 1) + 1;
+  @	axiom count_unmatched_last{L}:
+  @		\forall int *a, num, integer n;
+  @			\at(a[n - 1], L) != num ==> count{L}(a, num, n) == count{L}(a, num, n - 1);
   @ }
   @*/
-
-/*@ predicate swap{L1, L2}(int *a, integer i, integer j) =
-  @	\at(a[i], L1) == \at(a[j], L2) &&
-  @	\at(a[j], L1) == \at(a[i], L2) &&
-  @	\forall integer k; k != i && k != j ==> \at(a[k], L1) == \at(a[k], L2);
-  @
-  @ predicate sorted(int *a, integer first, integer last) =
+/*@ predicate sorted{L}(int *a, integer first, integer last) =
   @	\forall integer i;
-  @		(first < i < last ==> a[i - 1] <= a[i]);
+  @		first < i < last ==> \at(a[i - 1], L) <= \at(a[i], L);
   @*/
-
-/*@ requires \valid(a + i) && \valid(a + j);
-  @ assigns a[i], a[j];
-  @ ensures swap{Old, Here}(a, i, j);
+/*@ predicate greatest{L}(int *a, integer last) =
+  @ 	\forall integer i;
+  @		0 <= i < last ==> \at(a[i], L) <= \at(a[i], L);
   @*/
-void swap(int *a, int i, int j) {
-	int tmp = a[i];
-	a[i] = a[j];
-	a[j] = tmp;
-}
+/*@ axiomatic MULTISET_EQUAL {
+  @ 	logic boolean mequal{L1, L2}(int *a, integer n);
+  @	axiom mequal_refl{L}:
+  @		\forall int *a, integer n;
+  @			mequal{L, L}(a, n) == \true;
+  @	axiom mequal_sym{L1, L2}:
+  @		\forall int *a, integer n;
+  @			mequal{L1, L2}(a, n) ==> mequal{L2, L1}(a, n);
+  @	axiom mequal_trans{L1, L2, L3}:
+  @		\forall int *a, integer n;
+  @			mequal{L1, L2}(a, n) && mequal{L2, L3}(a, n) ==> mequal{L1, L3}(a, n);
+  @	axiom mequal{L1, L2}:
+  @		\forall int *a, i, n;
+  @			0 <= i < n ==>
+  @			mequal{L1, L2}(a, n) == (count{L1}(a, \at(a[i], L2), n) == count{L2}(a, \at(a[i], L2), n));
+  @ }
+  @*/
 
 /*@ requires n > 0;
   @ requires \valid(a + (0 .. n-1));
   @ 
   @ assigns a[0..n-1];
   @
-  @ ensures sorted(a, 0, n);
-  @ ensures permut{Old, Here}(a, 0, n);
+  @ ensures sorted{Here}(a, 0, n);
+  @ ensures mequal{Old, Here}(a, n);
   @*/
 void bubble_sort(int *a, int n) {
 	int i, j, tmp;
-	//@ ghost int g_swap = 0;
 	/*@ loop invariant 0 <= i <= n - 1;
-	  @ loop invariant 0 <= g_swap <= 1;
-	  @ loop invariant sorted(a, n - i - 1, n);
-	  @ loop invariant 0 < i < n ==> \forall int k, l; 0 <= k <= n - i - 1 <= l < n ==> a[k] <= a[l];
-	  @ loop invariant 0 < i < n ==> permut{Pre, Here}(a, 0, n);
-	  @ loop assigns i, j, a[0..n-1];
+	  @ loop invariant sorted{Here}(a, n - i - 1, n);
+	  @ loop invariant mequal{Pre, Here}(a, n);
+	  @ loop assigns i, j, tmp, a[0..n-1];
 	  @ loop variant n - i;
 	  @*/
 	for (i = 0; i < n - 1; ++i) {
-		//@ ghost g_swap = 0;
-		/*@ loop invariant bound: 0 <= j <= n - i - 1;
-		  @ loop invariant 0 <= g_swap <= 1;
-		  @ loop invariant 0 < j < n - i ==> \forall int k; 0 <= k < j ==> a[k] <= a[j];
-		  @ loop invariant 0 < j < n - i ==> (g_swap == 1) ==> permut{Pre, Here}(a, 0, n);
+		/*@ loop invariant bound: 0 <= j < n - i;
+		  @ loop invariant greatest{Here}(a, j);
+		  @ loop invariant mequal{Pre, Here}(a, n);
 		  @
-		  @ loop assigns j, a[0..n-i-1];
+		  @ loop assigns j, tmp, a[0..n-i-1];
 		  @ loop variant n - i - 1 - j;
 		  @*/
 		for (j = 0; j < n - i - 1; j++) {
 			if (a[j] > a[j + 1]) {
-				//@ ghost g_swap = 1;
-				swap(a, j, j + 1);
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
 			}
 		}
 	}
